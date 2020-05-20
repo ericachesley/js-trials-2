5fe627ab9353806a3ba327ad8222da136f992bad
Erica Chesley <erchesley@gmail.com>
2020-05-19 17:06:51 -0700

    Further study complete

--- furtherStudy.js
+++ furtherStudy.js
@@ -2,10 +2,64 @@
 
 
 function wordsInCommon(words1, words2) {
-  // Replace this with your code
+  const set1 = new Set(words1);
+  const set2 = new Set(words2);
+  const common = new Set();
+
+  for (const word of set1) {
+    if (set2.has(word)) {
+        common.add(word);
+    }
+  }
+
+  return common;
 }
+
+console.log(wordsInCommon(['Python', 'Python', 'Python'],
+                          ['Lizard', 'Turtle', 'Python']));
+
+console.log(wordsInCommon(['cheese', 'cheese', 'cheese', 'cake'],
+                          ['cheese', 'hummus', 'beets', 'cake']));
+
+console.log(wordsInCommon(['lamb', 'chili', 'cheese'],
+                          ['cake', 'ice cream']));
 
 
 function kidsGame(names) {
-  // Replace this with your code
+
+  let currentWord = names[0];
+  const output = [currentWord];
+  names = names.slice(1);
+
+  let firstLetterToWords = {};
+
+  for (const name of names) {
+    if (! firstLetterToWords.hasOwnProperty(name[0])) {
+        firstLetterToWords[name[0]] = [name];
+    } else {
+        firstLetterToWords[name[0]].push(name);
+    }
+  }
+
+  while (true) {
+    const startLetter = currentWord[currentWord.length-1];
+    if (! firstLetterToWords.hasOwnProperty(startLetter) ||
+        firstLetterToWords[startLetter].length === 0) {
+        break;
+    }
+    currentWord = firstLetterToWords[startLetter][0];
+    firstLetterToWords[startLetter] = firstLetterToWords[startLetter].slice(1);
+    output.push(currentWord);
+  }
+  return output;
 }
+
+
+console.log(kidsGame(["bagon", "baltoy", "yamask", "starly",
+                      "nosepass", "kalob", "nicky", "booger"]));
+
+console.log(kidsGame(["apple", "berry", "cherry"]));
+
+console.log(kidsGame(["noon", "naan", "nun"]));
+
+
